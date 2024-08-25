daf2ab87dd2c9b05a64bb8a6b87f5de29402ab21
Fra-G4 <botunknown23@gmail.com>
2024-08-25 17:05:42 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,19 +4,19 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "48459439078"
-global.ownername = "Toxxic Md"
+global.ownernomer = "2348102790119"
+global.ownername = "HYDRA'S killing bug"
 global.ytname = "Toxxic Md"
 global.socialm = "GitHub: Toxic1239"
 global.location = "Africa, Nigeria, Lagos"
 
 global.ownernumber = '48459439078'  //creator number
-global.ownername = 'Toxxic Md' //owner name
-global.botname = 'ᴢᴇɴᴏɴ ᴍᴅ' //name of the bot
+global.ownername = 'HYDRA killing bug' //owner name
+global.botname = 'HYDRA killing bug' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'ZENON\n\nContact: 48459439078'
+global.author = 'HYDRAkilling bug\n\nContact: +2348102790119'
 
 //console view/theme
 global.themeemoji = '💀'
